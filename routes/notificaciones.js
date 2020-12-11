ad8f08881fb6bfdf91d8be9ff13121f81d6a9ef2
Brian Vega <brian@tivelabs.com>
2020-12-11 05:01:26 -0400

    get grupo by id grupo

--- routes/notificaciones.js
+++ routes/notificaciones.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-const { getAll, getByIdUser, createNotifications, deleteById } = require('../controllers/notificaciones');
+const { getAll, getByIdUser, createNotifications, deleteById, deleteByIdUser } = require('../controllers/notificaciones');
 
 router.get('/', getAll);
 router.post('/', createNotifications);
