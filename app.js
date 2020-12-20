a7b9c5736b8d60b0127f57495a4908f548adbecd
Brian Vega <dentvega6@gmail.com>
2020-12-20 14:29:50 -0400

    last message

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ cron.schedule('50 * * * *', function() {
   }
 });
 
-cron.schedule('30 14 * * 7', async function() {
+cron.schedule('31 14 * * 7', async function() {
   try {
     const users = await getUsersLocal()
     users.forEach((user) => {
