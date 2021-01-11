73b8bbb07b3fbe5c58aae62a75cf99592b4bc26c
arnol <arnol01233@gmail.com>
2021-01-11 17:09:29 -0400

    password changed

--- controllers/pool.js
+++ controllers/pool.js
@@ -3,7 +3,7 @@ const { Pool } = require('pg');
 const pool = new Pool({
     host: 'bzv6zciyjbbxnovb9tgx-postgresql.services.clever-cloud.com',
     user: 'urjthqzsg7tlka1dw0dy',
-    password: 'Mz8ibq4hkvpOivMAHyJ5',
+    password: 'CiWwlRLtDds6hLiYfn45',
     database: 'bzv6zciyjbbxnovb9tgx',
     port: '5432'
 });
